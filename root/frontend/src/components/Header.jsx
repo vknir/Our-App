1d879a716f8afaf9bd04435d26c9d35492258e70
Utkarsh Kumar <utkarsh.kumar0110@gmail.com>
2024-11-03 18:41:48 +0530

    header component unsigned complete

--- root/frontend/src/components/Header.jsx
+++ root/frontend/src/components/Header.jsx
@@ -1,20 +1,32 @@
-import './Header.css'
-import { NavLink } from 'react-router-dom'
+import axios from "axios";
 
+import "./Header.css";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
-function Header(){
-    return <nav>
-        <NavLink to='/'>
-            <p>Our App</p>
+function Header() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
+  return (
+    <nav>
+      <div className="left-section">
+        <NavLink to="/">
+          <p>OurApp</p>
         </NavLink>
+      </div>
 
-
-        <form>
-            <input type='text' placeholder='Username'></input>
-            <input type='password' placeholder='Password'></input>
-            <button type='submit'>Login</button>
+      <div className="right-section">
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Username"></input>
+          <input type="password" placeholder="Password"></input>
+          <button type="submit">Login</button>
         </form>
+      </div>
     </nav>
+  );
 }
 
-export default Header
+export default Header;
