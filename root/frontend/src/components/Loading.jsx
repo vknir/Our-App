9fb0846719ed44414b5ccf1149d4c890ebcb21dd
vknir <kutkarsh.umar@gmail.com>
2024-11-06 10:10:14 +0530

    restructured components

--- root/frontend/src/components/Loading.jsx
+++ root/frontend/src/components/Loading.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 
-import './Loading.css'
+import './style/Loading.css'
 
 
 function Loading()
