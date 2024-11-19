257684ce0db59902c6b426b1cc19c709d7a07a7b
vknir <kutkarsh.umar@gmail.com>
2024-11-19 23:29:29 +0530

    start with chat faeture, install Socket.io on backend

--- root/backend/index.js
+++ root/backend/index.js
@@ -1,16 +1,29 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import {createServer} from 'http'
+import { Server } from "socket.io";
 import userRouter from "../backend/routes/user.js";
 import pRouter from "./routes/p.js";
 import { MONGO_URL } from "../backend/config.js";
 
 const app = express();
+const httpServer = createServer(app);
+const io=new Server(  httpServer,{})
 const port = process.env.PORT || 3000;
+const port2= process.env.PORT || 3001;
+
+io.on('connection', (socket)=>{
+
+})
 
 async function main() {
   await mongoose.connect(MONGO_URL);
+  
+  httpServer.listen(port2, ()=>{
+    console.log(`httpserver io listening on ${port2}`)
+  })
+  
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
